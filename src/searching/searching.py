7d031fdf8dbebf084e3302f8ac461fb66a0fc590
Michael Phelps <michaeljphelps1978@gmail.com>
2020-10-02 00:19:51 -0500

    :zap: Sorting
    Merge-Sort and Merge complete and tested.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -8,14 +8,15 @@ def binary_search(arr, target, start, end):
     # Your code here
     if len(arr) <= 0:  # edge case for empty array
         return -1
+
     # get the middle of the dataset
     middle = math.floor((start + end) / 2)  # using the floor operator to round down
 
-    if arr[middle] == target:  # first, if the middle IS the target, we're done
+    if arr[middle] == target:  # first, if the middle IS the target, we're done - This is also our Base Case
         return middle
 
     elif arr[middle] < target:  # target is less than the middle
-        # r ecursive call, moving the start point to 1 after the middle
+        # recursive call, moving the start point to 1 after the middle
         return binary_search(arr, target, middle + 1, end)
 
     elif arr[middle] > target:  # target is greater than the middle
